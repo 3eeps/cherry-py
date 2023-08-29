5a8e653cb1f5622b9894bf1fd8a86df1571c4612
3eeps <55860052+3eeps@users.noreply.github.com>
2023-08-29 11:20:24 -0400

    Update convert_pipeline.py

--- convert_pipeline.py
+++ convert_pipeline.py
@@ -1,6 +1,14 @@
 # ./codespace/convert_pipeline.py
 import os
 
+def quant_gguf():
+    arg_list_quant = ['gguf_file', 'output_file', 'quant type']
+    iter = -1
+    for arg in arg_list_quant:
+        iter = iter + 1
+        arg_list_quant[iter] = input(f"{arg}: ")
+    os.system(f"quantize.exe {arg_list_quant[0]} {arg_list_quant[1]} {arg_list_quant[2]}")
+
 def hf_to_gguf():
     arg_list_hf = ['hf_dir']
     iter = -1
@@ -18,14 +26,15 @@ def ggml_to_gguf():
     os.system(f"py convert_ggml_to_gguf.py --input {arg_list_ggml[0]} --output {arg_list_ggml[1]} --model-metadata-dir {arg_list_ggml[2]}")
 
 run_loop = True
+print("'hf': hf-to-gguf")
+print("'ggml': ggml-gguf")
+print("'quant': quantize-gguf")
 while run_loop:
-    convert_type = input("convert from 'hf' to gguf, or 'ggml' to gguf?")
-
+    convert_type = input(">>> ")
     if convert_type == "hf":
         hf_to_gguf()
     elif convert_type == "ggml":
         ggml_to_gguf()
+    elif convert_type == "quant":
+        quant_gguf()
     run_loop = False
-
-
-# add option to quant it if this all works
